cf886206bb1633c3cf2f33709e249165d2de0d1f
MadImp <60294415+Mad-Imp@users.noreply.github.com>
2020-09-17 21:23:48 +0300

    Алгоритм сортировки пузырьком

---
+++ sort/BubbleSort.js
@@ -0,0 +1,19 @@
+let someArr = [12, 5, 92, 23, 8, 2];
+
+let count = someArr.length - 1;
+
+for (let i = 0, k = 0; i < count; count--) {
+	for (let j = 0; j < count; j++) {
+		if (someArr[j] > someArr[j + 1]) {
+			let temp = someArr[j];
+			someArr[j] = someArr[j + 1];
+			someArr[j + 1] = temp;
+			k++;
+		}
+	}
+	if (k == 0) {
+		break;
+	} 
+}
+
+console.log(someArr);
